ae75d12b69a26642dba9414d9bb47d8c5863a7e7
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-10-13 11:28:30 +0100

    update task 02js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,17 +1,25 @@
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// const listLitEl = document.querySelector('#ingredients');
+
+// const el = ingredients.map(ingredient => {
+//   const liEl = document.createElement('li');
+//   liEl.classList.add('item');
+//   liEl.textContent = ingredient;
+
+//   return liEl;
+// });
+
+// console.log(el);
+// listLitEl.append(...el);
+
 // HTML містить порожній список ul#ingredients.
 
 // <ul id="ingredients"></ul>
 
 // JavaScript містить масив рядків.
 
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatos",
-//   "Herbs",
-//   "Condiments",
-// ];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 // Напиши скрипт, який для кожного елемента масиву ingredients:
 
@@ -19,18 +27,12 @@
 // Додасть назву інгредієнта як його текстовий вміст.
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+const ingredientsEl = document.querySelector(`#ingredients`);
 
-const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
-
-const listLitEl = document.querySelector('#ingredients');
-
-const el = ingredients.map(ingredient => {
+const ingredientEl = ingredients.map(ingredient => {
   const liEl = document.createElement('li');
   liEl.classList.add('item');
   liEl.textContent = ingredient;
-
   return liEl;
 });
-
-console.log(el);
-listLitEl.append(...el);
+ingredientsEl.append(...ingredientEl);
